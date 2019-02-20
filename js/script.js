09ad8e03f1d74180fffadf6c7451cf87dc80be6a
Andrew <Leadfall@ex.ua>
2019-02-20 21:55:48 +0200

    Добавление функционала 'Избранное'(не работает 'добавить в корзину' из избранного)

--- js/script.js
+++ js/script.js
@@ -23,6 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 <div class="goods__price">
                     <span>${item.price}</span> руб
                 </div>
+                <button class="goods__favorite">В избранное</button>
                 <button class="goods__btn">Добавить в корзину</button>
             `;
             goodsWrapper.appendChild(card);
@@ -30,18 +31,86 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     loadContent('js/db.json', () => {
+
     // Переменные
-        const cartWrapper = document.querySelector('.cart__wrapper'),
+        const favorWrapper = document.querySelector('.favorite__wrapper'),
+            cartWrapper = document.querySelector('.cart__wrapper'),
+            favorite = document.querySelector('.favorite'),
+            closefavorite = document.querySelector('.favorite__close'),
+            openfavorite = document.querySelector('#favorite'),
             cart = document.querySelector('.cart'),
             close = document.querySelector('.cart__close'),
             open = document.querySelector('#cart'),
+            goodsFavorite = document.querySelectorAll('.goods__favorite'),
             goodsBtn = document.querySelectorAll('.goods__btn'),
             products = document.querySelectorAll('.goods__item'),
             confirm = document.querySelector('.confirm'),
+            badgefavor = document.querySelector('.nav__badge__favor'),
             badge = document.querySelector('.nav__badge'),
             totalCost = document.querySelector('.cart__total > span'),
             titles = document.querySelectorAll('.goods__title');
 
+// Favorite Module -------------------------------------------
+    // Открытие|закрытие окна избранных
+    function openFavorite() {
+        favorite.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    }
+    function closeFavorite() {
+        favorite.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+    openfavorite.addEventListener('click', openFavorite);
+    closefavorite.addEventListener('click', closeFavorite);
+
+    
+    // Добавление товара в избранные
+    goodsFavorite.forEach(function (favor, i) {
+        favor.addEventListener('click', () => {
+            let itemfavor = products[i].cloneNode(true),
+                trigger = itemfavor.querySelector('button'),
+                removeBtnFavor = document.createElement('div'),
+                empty = favorWrapper.querySelector('.empty');
+            trigger.remove();
+
+            removeBtnFavor.classList.add('goods__item-remove');
+            removeBtnFavor.innerHTML = '&times';
+            itemfavor.appendChild(removeBtnFavor);
+
+            favorWrapper.appendChild(itemfavor);
+            if (empty) {
+                empty.style.display = 'none';
+            }
+
+            calcFavorite();
+            removeFromFavorite();
+        });
+    });
+
+    // Счетчик к-ва товаров в избранном    
+    function calcFavorite() {
+        const items = favorWrapper.querySelectorAll('.goods__item');
+        badgefavor.textContent = items.length;
+
+        let empty = favorWrapper.querySelector('.empty');
+        if (badgefavor.textContent < 1) {
+            empty.style.display = 'block';
+        }
+    }
+
+    // Удаление товара из избранных    
+    function removeFromFavorite() {
+        const removeBtnFavor = favorite.querySelectorAll('.goods__item-remove');
+        removeBtnFavor.forEach(function (favor) {
+            favor.addEventListener('click', () => {
+                favor.parentElement.remove();
+
+                calcFavorite();
+            });
+        });
+    }
+
+// Cart Module -------------------------------------------
     // Открытие|закрытие окна корзины
         function openCart() {
             cart.style.display = 'block';
@@ -147,13 +216,3 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 });
-
-
-// Promise
-// fetch('https://jsonplaceholder.typicode.com/posts', 
-//     {
-//         method: "POST",
-//         body: JSON.stringify(example);
-//     })
-//   .then(response => response.json())
-//   .then(json => console.log(json))
